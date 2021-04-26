bf1c2e3714f13e0060e51e485fd589d3d9bc1735
Benjamin <67079423+NegativeDivinity@users.noreply.github.com>
2021-04-26 09:15:32 -0500

    pic add

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch, Component} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./style.css";
 
 // Pages
-import Home from './pages/home';
-import Thank from './pages/thank';
-import About from './pages/about';
-import Work from './pages/work';
+import Home from "./pages/home";
+import Thank from "./pages/thank";
+import About from "./pages/about";
+import Work from "./pages/work";
 
 export default function App() {
   return (
     <div>
       <Router>
         <Switch>
-          <Route exact path = '/' component = {Home} />
-          <Route exact path = '/about' component = {About} />
-          <Route exact path = '/work' component = {Work} />
-          <Route exact path = '/thank' component = {Thank} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/work" component={Work} />
+          <Route exact path="/thank" component={Thank} />
         </Switch>
       </Router>
     </div>
